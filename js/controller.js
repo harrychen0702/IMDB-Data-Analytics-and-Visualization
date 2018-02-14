67104cb9df0546fe0c5b6e81bae7ef337f37565f
harrychen <chenhao201509@163.com>
2018-02-14 13:54:50 +0000

    Add graph libraries

--- js/controller.js
+++ js/controller.js
@@ -1,4 +1,4 @@
-angular.module('IMDB.controllers', ['ngTable'])
+angular.module('IMDB.controllers', ['ngTable','chart.js'])
 
 .controller('HomeCtrl', ['$scope','$state','$http',function ($scope,$state,$http) {
    $scope.types=["General Search","By ID","By Title"];
@@ -54,6 +54,15 @@ angular.module('IMDB.controllers', ['ngTable'])
    $scope.test=function(){
     console.log("hello");
    }
+
+   $scope.labels = ['2006', '2007', '2008', '2009', '2010', '2011', '2012'];
+   $scope.series = ['Series A', 'Series B'];
+
+   $scope.data = [
+     [65, 59, 80, 81, 56, 55, 40],
+     [28, 48, 40, 19, 86, 27, 1000]
+   ];
+
 }])
 
 .controller('search_resultCtrl', ['$scope','$http','$stateParams','$state',function ($scope,$http,$stateParams,$state) {
